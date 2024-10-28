ee79d222ce368de5a03a3741e7109e1d3f93ab06
youyu <129youyu@gmail.com>
2024-10-28 10:48:19 +0800

    新增BMI表單

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
-hello
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>計算BMI</h1>
+    <form action="result.php" method="get">
+        <!-- 換行：<br>是早期的用法 現在多用<div> -->
+        <div>
+            <label for="height">身高：</label>
+            <input type="number" name="height" id="height">
+        </div>
+        <div>
+            <label for="weight">體重：</label>
+            <input type="number" name="weight" id="weight">
+        </div>
+        <div>
+            <!-- input:submit+input+reset -->
+            <input type="submit" value="計算">
+            <input type="reset" value="清空/重置">
+        </div>
+    </form>
+</body>
+</html>
