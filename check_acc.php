e0336c5d8fece8e371818b8cf8040285acb7fb25
youyu <129youyu@gmail.com>
2024-10-28 14:33:26 +0800

    新增登入頁面及帳號密碼驗證

--- check_acc.php
+++ check_acc.php
@@ -7,7 +7,14 @@
 </head>
 <body>
     <?php
-    echo "您的帳號密碼正確"
+    $acc=$_POST['acc'];
+    $pw=$_POST['pw'];
+
+    if($acc=='admin' && $pw=='1234'){
+        echo"帳密正確，登入成功";
+        }else{
+            echo "帳密錯誤，登入失敗";
+    }
     ?>
 </body>
 </html>
--- login.php
+++ login.php
@@ -4,20 +4,65 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f2f2f2;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+        h2 {
+            text-align: center;
+            margin-bottom: 20px;
+        }
+        .login-container {
+            background-color: white;
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            width: 300px;
+        }
+        input[type="text"],
+        input[type="password"] {
+            width: 80%;
+            padding: 10px;
+            margin-bottom: 15px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+        }
+        input[type="submit"] {
+            display: block;
+            margin:auto;
+            padding: 10px 20px;
+            background-color: #0072E3;
+            color: white;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+            font-size: 16px;
+        }
+    </style>
 </head>
 <body>
-    <form action="login2.php">
+    <div class="login-container">
+        <h2>登入</h2>
+    <form action="check_acc.php" method="post">
         <div>
-            <input type="text" name="account" id="account">
             <label for="帳號">帳號</label>
+            <input type="text" name="acc" id="acc">
         </div>
         <div>
-            <input type="text" name="password" id="password">
             <label for="密碼">密碼</label>
+            <input type="password" name="pw" id="pw">
         </div>
+        <P></P>
         <div>
             <input type="submit" value="登入">
         </div>
     </form>
+    </div>
 </body>
 </html>
