5a29640bda79042a9ad18e2f30496d0a80867f74
youyu <129youyu@gmail.com>
2024-10-30 16:40:19 +0800

    新增用迴圈產生nav

--- include/common/navbar.php
+++ include/common/navbar.php
@@ -9,10 +9,7 @@ $header = [
 ?>
 <h1><?=$header[$page];?></h1>
 
-<div>
-<a class="<?=($page=='index')?'now-page':'';?>" href="index.php">首頁</a>
-<a class="<?=($page=='about')?'now-page':'';?>" href="about.php">關於我們</a>
-<a class="<?=($page=='product')?'now-page':'';?>" href="product.php">產品介紹</a>
-<a class="<?=($page=='contact')?'now-page':'';?>" href="contact.php">聯絡我們</a>
-<a class="<?=($page=='login')?'now-page':'';?>" href="login.php">登入</a>
-</div>
+<?php
+foreach($header as $key => $value){
+    echo "<a class='".($page==$key?'now-page':'')."' href='{$key}.php'>{$value}</a>
+}
