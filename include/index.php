0dfa1c279fff51bcaec52b3f3496a09fd600884c
youyu <129youyu@gmail.com>
2024-10-30 15:33:11 +0800

    新增include_五頁網站

---
+++ include/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>首頁</h1>
+    <a href="index.php">首頁</a>
+    <a href="about.php">關於我們</a>
+    <a href="product.php">產品介紹</a>
+    <a href="contact.php">聯絡我們</a>
+    <a href="login.php">登入</a>
+
+    <main>
+        <h2>歡迎光臨</h2>
+        <P>這是一個網站的首頁</P>
+    </main>
+    <footer>
+        <P>版權所有</P>
+    </footer>
+</body>
+</html>
---
+++ include/product.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>產品介紹</h1>
+    <a href="index.php">首頁</a>
+    <a href="about.php">關於我們</a>
+    <a href="product.php">產品介紹</a>
+    <a href="contact.php">聯絡我們</a>
+    <a href="login.php">登入</a>
+    <main>
+        <h2>我們的產品</h2>
+        <P>我們提供最好的產品</P>
+    </main>
+    <footer>
+        <P>版權所有</P>
+    </footer>
+</body>
+</html>
