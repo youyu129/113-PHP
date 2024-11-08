33c94bfe264003e74e7f6070e2bea73ab560b1ce
youyu <129youyu@gmail.com>
2024-11-08 15:56:11 +0800

    修改pdo呈現方式為表格

---
+++ classes_detail.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>[班級]詳細資料]</title>
+</head>
+<body>
+    <h1>班級學員</h1>
+</body>
+</html>
--- pdo.php
+++ pdo.php
@@ -1,13 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            border: 1px solid black;
+            margin:auto;
+        }
+        th{
+            background-color: #fff;
+            color:darkorange;
+            text-shadow:;
+        }
+        tr,
+        td {
+            height: 30px;
+            border: 1px solid black;
+            padding: 5px 15px;
+            text-align:center;
+        }
+    </style>
 </head>
+
 <body>
     <h1>資料庫連線</h1>
-    <?php
+    <table>
+    <tr>
+        <td>序號</td></td><td>班級名稱</td><td>班導師</td>
+    </tr>
+        <?php
     // $dsn通常都用這個變數
     // dbname資料庫名稱
     // port 資料庫用3306 web用80 可以不用寫
@@ -19,13 +44,19 @@
     $sql="select * from classes";
     $rows=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 
+    
     foreach ($rows as $row) {
-        echo $row['id']."-".$row['name']."-".$row['tutor']."<br>";
-    }
-
-    // echo "<pre>";
-    // print_r($rows);
-    // echo "</pre>";
     ?>
+        <tr>
+            <td><?=$row['id'];?></td>
+            <td><?=$row['name']?></td>
+            <td><?=$row['tutor']?></td>
+        </tr>
+    <?php
+    }
+    ?>
+
+    </table>
 </body>
+
 </html>
