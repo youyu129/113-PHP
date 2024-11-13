4dcfbcc535acd7e099fe2cf1a17f2b1d8a3a5171
youyu <129youyu@gmail.com>
2024-11-13 15:47:34 +0800

    新增刪除資料的功能

---
+++ del.php
@@ -0,0 +1,18 @@
+<?php
+
+// 沒有id或id不是數字
+if (!isset($_GET['id']) || !is_numeric($_GET['id'])){
+    echo "非法使用";
+    exit();
+}
+
+$dsn="mysql:host=localhost;charset=utf8;dbname=crud";
+$pdo=new PDO ($dsn,'root','');
+
+$id=$_GET['id'];
+$sql="delete from member where id='$id'";
+
+$pdo->exec($sql);
+
+header("location:success.php");
+?>
--- success.php
+++ success.php
@@ -7,6 +7,52 @@
 </head>
 <body>
     <h1>登入成功</h1>
+
+<?php
+$dsn="mysql:host=localhost;charset=utf8;dbname=crud";
+$pdo=new PDO($dsn,'root','');
+
+$rows=$pdo->query("select * from member")->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+    <table>
+        <tr>
+            <td>id</td>
+            <td>acc</td>
+            <td>pw</td>
+            <td>email</td>
+            <td>tel</td>
+            <td>操作</td>
+        </tr>
+<?php
+foreach($rows as $row){
+?>
+
+        <tr>
+            <td><?=$row['id'];?></td>
+            <td><?=$row['acc'];?></td>
+            <td><?=$row['pw'];?></td>
+            <td><?=$row['email'];?></td>
+            <td><?=$row['tel'];?></td>
+            <td>
+                <a href="edit.php?id=<?=$row['id'];?>">編輯</a>
+                <a href="del.php?id=<?=$row['id'];?>">刪除</a>
+            </td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+
+        <?php
+}
+?>
+    </table>
+
     <div>
         <a href="index.html">回首頁</a>
     </div>
